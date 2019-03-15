5e9c3d6b9a7849eac8d51fa0c88ec69cd1717008
Thomas Sander <thomas.sander@actelion.com>
2019-03-15 17:34:14 +0100

    fixed issues around cropping and minimization

--- src/org/openmolecules/fx/viewer3d/V3DMinimizer.java
+++ src/org/openmolecules/fx/viewer3d/V3DMinimizer.java
@@ -9,18 +9,27 @@ import javafx.application.Platform;
 import javafx.geometry.Point3D;
 import javafx.scene.Node;
 import mmff.ForceFieldMMFF94;
-import org.openmolecules.mesh.MoleculeSurfaceMesh;
+import org.openmolecules.mesh.MoleculeSurfaceAlgorithm;
 
 import java.util.ArrayList;
 
 public class V3DMinimizationHandler implements ForceFieldChangeListener {
+	private static V3DMinimizationHandler sInstance;
 	private V3DMolecule[] mFXMol;
+	private V3DMoleculeUpdater[] mFXMolUpdater;
 	private ForceField mForceField;
 	private V3DScene mScene3D;
 	private V3DSceneEditor mEditor;
 	private volatile Thread mMinimizationThread;
-	
-	public V3DMinimizationHandler(V3DScene scene3D, V3DSceneEditor editor)  {
+
+	public V3DMinimizationHandler getInstance(V3DScene scene3D, V3DSceneEditor editor) {
+		if (sInstance == null)
+			sInstance = new V3DMinimizationHandler(scene3D, editor);
+
+		return sInstance;
+	}
+
+	private V3DMinimizationHandler(V3DScene scene3D, V3DSceneEditor editor)  {
 		mScene3D = scene3D;
 		mEditor = editor;
 
@@ -50,12 +59,17 @@ public class V3DMinimizationHandler implements ForceFieldChangeListener {
 		if (mFXMol.length == 0)
 			return;
 
+		mFXMolUpdater = new V3DMoleculeUpdater[mFXMol.length];
+		for (int i=0; i<mFXMol.length; i++)
+			mFXMolUpdater[i] = new V3DMoleculeUpdater(mFXMol[i]);
+
 		StereoMolecule molScenery = new StereoMolecule();
 
 		int atom = 0;
+		ArrayList<Integer> rigidAtoms = new ArrayList<>();
 		for(V3DMolecule fxmol : mFXMol) {
 			// remove any surfaces
-			for (int type=0; type<MoleculeSurfaceMesh.SURFACE_TYPE.length; type++)
+			for (int type = 0; type<MoleculeSurfaceAlgorithm.SURFACE_TYPE.length; type++)
 				fxmol.setSurfaceMode(type ,V3DMolecule.SURFACE_NONE);
 
 			Conformer conf = fxmol.getConformer();
@@ -67,6 +81,10 @@ public class V3DMinimizationHandler implements ForceFieldChangeListener {
 				molScenery.setAtomX(atom, globalCoords.getX());
 				molScenery.setAtomY(atom, globalCoords.getY());
 				molScenery.setAtomZ(atom, globalCoords.getZ());
+				if (mol.isMarkedAtom(a))
+					rigidAtoms.add(atom);
+				if (mol.getAtomicNo(a) == 0)
+					molScenery.setAtomicNo(atom, 1);
 				atom++;
 			}
 		}
@@ -74,28 +92,37 @@ public class V3DMinimizationHandler implements ForceFieldChangeListener {
 		ForceFieldMMFF94.initialize(ForceFieldMMFF94.MMFF94SPLUS);
 		mForceField = new ForceFieldMMFF94(molScenery, ForceFieldMMFF94.MMFF94SPLUS);
 		mForceField.addListener(this);
+		int[] fixedAtom = new int[rigidAtoms.size()];
+		for (int i=0; i<rigidAtoms.size(); i++)
+			fixedAtom[i] = rigidAtoms.get(i);
+		mForceField.setFixedAtoms(fixedAtom);
 
 		mMinimizationThread = new Thread(() -> mForceField.minimise());
 		mMinimizationThread.start();
 	}
 
-	public void stopMinimization() {
-		mMinimizationThread = null;
-		mForceField.interrupt();
+	public static void stopMinimization() {
+		if (sInstance != null && sInstance.mMinimizationThread != null) {
+			sInstance.mMinimizationThread = null;
+			sInstance.mForceField.interrupt();
+			sInstance = null;
+		}
 	}
 
 	@Override
 	public void stateChanged() {
-		final double[] pos = mForceField.getCurrentPositions();
-		final double energy = mForceField.getTotalEnergy();
-		Platform.runLater(() -> {
-			if (mMinimizationThread != null) {
-				mScene3D.updateCoordinates(mFXMol, pos);
-				if (mEditor != null)
-					mEditor.createOutput("energy: " + Double.toString(energy) + " kcal/mol");
-				else
-					System.out.println("MMFF-Energy: "+ DoubleFormat.toString(energy) + " kcal/mol");
-			}
-		});
+		if (mMinimizationThread != null) {
+			final double[] pos = mForceField.getCurrentPositions();
+			final double energy = mForceField.getTotalEnergy();
+			Platform.runLater(() -> {
+				if (mMinimizationThread != null) {
+					mScene3D.updateCoordinates(mFXMol, pos);
+					if (mEditor != null)
+						mEditor.createOutput("energy: " + Double.toString(energy) + " kcal/mol");
+					else
+						System.out.println("MMFF-Energy: "+ DoubleFormat.toString(energy) + " kcal/mol");
+				}
+			});
+		}
 	}
 }
