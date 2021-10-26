7277b90402bfdbf043b63f1f9877d4b17b807990
wahljo1 <wahljo1@CHALW-L999961.idorsia.com>
2021-10-26 15:55:36 +0200

    now also visualizes torsion strains of macrocycles

--- src/org/openmolecules/fx/viewer3d/torsionstrain/V3DTorsionStrainAnalyzer.java
+++ src/org/openmolecules/fx/viewer3d/torsionstrain/V3DTorsionStrainAnalyzer.java
@@ -9,6 +9,7 @@ import org.openmolecules.fx.viewer3d.MolStructureChangeListener;
 import org.openmolecules.fx.viewer3d.V3DMolecule;
 
 import com.actelion.research.chem.StereoMolecule;
+import com.actelion.research.chem.conf.BondRotationHelper;
 import com.actelion.research.chem.conf.TorsionDB;
 
 import javafx.beans.InvalidationListener;
@@ -25,8 +26,9 @@ public class V3DTorsionStrainAnalyzer implements MolStructureChangeListener{
 		fxmol.addMoleculeStructureChangeListener(this);
 		torsionAngles = new HashMap<Integer,TorsionAngle>();
 	}
-	
+	/*
 	public void init() {
+		BondRotationHelper rotHelper = BondRotationHelper(fxmol.getMolecule());
 		for(int b=0;b<fxmol.getMolecule().getBonds();b++) {
 			for(int rotBondIndex=0;rotBondIndex<fxmol.getBondRotationHelper().getRotatableBonds().length;rotBondIndex++) {
 				int rotBond = fxmol.getBondRotationHelper().getRotatableBonds()[rotBondIndex];
@@ -39,6 +41,20 @@ public class V3DTorsionStrainAnalyzer implements MolStructureChangeListener{
 			}
 		}
 			
+	}
+	*/
+	
+	public void init() {
+		boolean[] isRotatableBond = new boolean[fxmol.getMolecule().getBonds()];
+		TorsionDB.findRotatableBonds(fxmol.getMolecule(),false, isRotatableBond);
+		for(int b=0;b<isRotatableBond.length;b++) {
+			if(!isRotatableBond[b])
+				continue;
+			int[] torsionAtom = new int[4];
+			String torsionID = TorsionDB.getTorsionID(fxmol.getMolecule(), b, torsionAtom, null);
+			TorsionAngle torsionAngle = new TorsionAngle(torsionAtom, torsionID);
+			torsionAngles.put(b, torsionAngle);
+		}
 	}
 
 
@@ -74,6 +90,8 @@ public class V3DTorsionStrainAnalyzer implements MolStructureChangeListener{
 			double angle = TorsionDB.calculateTorsionExtended(mol,atoms);
 			return TorsionDB.getTorsionStrainClass(torsionID, angle);
 		}
+		
+		
 	}
 
 	
