df3953b5e3639b0f116f5d9ac211b8299cbe0415
Mark Williamson <mjw@mjw.name>
2022-07-25 13:26:26 +0100

    Initial modules attempt

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module fxmolviewer {
+	requires java.desktop;
+	requires javafx.controls;
+	requires javafx.base;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires openchemlib;
+	requires janino;
+	requires commons.compiler;
+	requires mmtf.codec;
+	requires mmtf.serialization;
+	requires mmtf.api;
+
+}
