2a007f680f538f93241015f7b9cca92c3060d396
thomas <thomas@openmolecules.org>
2026-06-17 21:13:42 +0200

    introduced neutral rf range from 0.5 to 1.0

--- src/main/java/org/openmolecules/fx/viewer3d/interactions/rf/RFInteractionV3D.java
+++ src/main/java/org/openmolecules/fx/viewer3d/interactions/rf/RFInteractionV3D.java
@@ -6,11 +6,14 @@ import org.openmolecules.fx.viewer3d.interactions.V3DInteraction;
 import org.openmolecules.fx.viewer3d.interactions.V3DInteractionPoint;
 
 public class RFInteractionV3D extends V3DInteraction {
+	private static final double LOW_NEUTRAL_RF = 0.5;
+	private static final double HIGH_NEUTRAL_RF = 1.0;
 	private final RFInteraction mInteraction;
 
 	public RFInteractionV3D(V3DInteractionPoint ip1, V3DInteractionPoint ip2, RFInteraction interaction, double rf) {
-		super(ip1, ip2, rf, interaction.getDistance(), 0.5 + Math.abs(Math.log10(rf)),
-				(rf<0.5) ? Color.RED.darker() : (rf<1.0) ? Color.GRAY : Color.BLUE.brighter());
+		super(ip1, ip2, rf, interaction.getDistance(),
+				0.5 + Math.abs(Math.log10(rf < LOW_NEUTRAL_RF ? LOW_NEUTRAL_RF/rf : rf < HIGH_NEUTRAL_RF ? 1.0 : rf/HIGH_NEUTRAL_RF)),
+				(rf<LOW_NEUTRAL_RF) ? Color.RED.darker() : (rf<HIGH_NEUTRAL_RF) ? Color.GRAY : Color.BLUE.brighter());
 		mInteraction = interaction;
 	}
 
